a86074c1a78490a6d3aa31e70b416c37c9a40d18
Felipe Oliveira <felipehlvo@gmail.com>
2022-11-02 13:15:57 -0300

    added the poverty file

--- quiz_game.py
+++ quiz_game.py
@@ -4,6 +4,7 @@ from pywebio.input import *
 import sys as s
 from typing import Tuple, List
 import plotly.express as px
+import pandas as pd
 
 df = px.data.gapminder()
 fig = px.scatter(df, x="gdpPercap", y="lifeExp", 
@@ -11,6 +12,10 @@ fig = px.scatter(df, x="gdpPercap", y="lifeExp",
                  title="Log-transformed fit on linear axes")
 html = fig.to_html(include_plotlyjs="require", full_html=False)
 
+# Import csv from url
+df_test = pd.read_csv("https://raw.githubusercontent.com/berdikhanova/DS4SG-Global-Inequality/main/Data/Raw/test_data%20-%20Sheet1.csv")
+fig1 = px.scatter(df_test, x="column_1", y="column_2")
+html1 = fig1.to_html(include_plotlyjs="require", full_html=False)
 
 def main():
     name = checkbox("Who has the highest inequality?", options = ["Brazil", "Kazakhstan"])
@@ -21,7 +26,7 @@ def main():
         put_text(f"You chose {name}")
         put_text("Not quite")
     put_text("Now look at this graph and learn.")
-    put_html(html)
+    put_html(html1)
 
 if __name__ == '__main__':
     main()
