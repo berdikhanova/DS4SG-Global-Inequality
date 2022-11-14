570edb7d01e5a0ec888428546284c9932b98d671
felipehlvo <felipehlvo@gmail.com>
2022-11-14 17:32:15 -0300

    Updated organization of quiz app

--- Code/visualizations.py
+++ Code/visualizations.py
@@ -2,8 +2,10 @@ import plotly.express as px
 import pandas as pd
 import matplotlib.pyplot as plt
 
+df = pd.read_csv("https://github.com/berdikhanova/DS4SG-Global-Inequality/blob/Assignment/Data/Final/indicators.csv?raw=true")
+
 # Plot birth registration in Brazil over time with plotly express
-def brazil_gdp(df):
+def brazil_gdp():
     """
     Plots GDP per capita in Brazil over time
     """
--- quiz_game.py
+++ quiz_game.py
@@ -17,54 +17,23 @@ import matplotlib.pyplot as plt
 # Visualizations
 from Code.visualizations import *
 
+# Questions
+from Code.questions import *
+
 # Import csv from url
-df = pd.read_csv("https://github.com/berdikhanova/DS4SG-Global-Inequality/blob/Assignment/Data/Final/indicators.csv?raw=true")
+# df = pd.read_csv("https://github.com/berdikhanova/DS4SG-Global-Inequality/blob/Assignment/Data/Final/indicators.csv?raw=true")
 
 #### MAKE LANDING PAGE ####
-
-
-
-
-def clear_page():
-    clear()
-
-def question2():
-    clear_page()
-    put_text("This is question 2")
-    answer = checkbox("Has GDP per capita increased or decreased in Brazil?", options = ["Increased", "Decreased", "Stayed the same"])
-    
-    if answer[0] == "Increased":
-        put_text("That's right!")
-    else:
-        put_text("Not quite")
-
-    graph = brazil_gdp(df)
-    
-    put_html(graph)
-    put_text("As you can see in this graph, although there were some fluctutions, Brazil's GDP per capita has increased over time.")
-
-
-def question1():
-    clear_page()
-    answer = checkbox("Has GDP per capita increased or decreased in Brazil?", options = ["Increased", "Decreased", "Stayed the same"])
-    
-    if answer[0] == "Increased":
-        put_text("That's right!")
-    else:
-        put_text("Not quite")
-
-    graph = brazil_gdp(df)
-    put_html(graph)
-    put_text("As you can see in this graph, although there were some fluctutions, Brazil's GDP per capita has increased over time.")
 
 
 
 def main():
 
     question1()
+    
 
-    put_button("Next", onclick=question2)
-
+    
 
 if __name__ == '__main__':
-    start_server(main, debug=True, remote_access=True)
+    main()
+    #start_server(main, debug=True, remote_access=True)
