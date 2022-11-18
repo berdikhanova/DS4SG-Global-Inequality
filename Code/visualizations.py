276889267369107909cd9a70ebf2962b159161d9
felipehlvo <felipehlvo@gmail.com>
2022-11-18 17:14:10 -0300

    Added visualizations - Felipe

--- Code/visualizations.py
+++ Code/visualizations.py
@@ -5,19 +5,97 @@ import matplotlib.pyplot as plt
 df = pd.read_csv("https://github.com/berdikhanova/DS4SG-Global-Inequality/blob/Assignment/Data/Final/indicators.csv?raw=true")
 df2 = pd.read_csv("https://raw.githubusercontent.com/berdikhanova/DS4SG-Global-Inequality/Assignment/Data/Final/df_countries.csv")
 # Plot birth registration in Brazil over time with plotly express
-def brazil_gdp():
-    """
-    Plots GDP per capita in Brazil over time
-    """
-    # Subsetting data
-    brazil_gdp = df[(df['Country Code'] == 'BRA') & (df['Indicator Code'] == 'NY.GDP.PCAP.CD')]
-    brazil_gdp = brazil_gdp[pd.to_numeric(brazil_gdp['Date'], errors='coerce').notnull()]
 
-    # Plotting
-    fig = px.line(brazil_gdp, x="Date", y="value", title='GDP per capita in Brazil')
-    
+def income_distribution():
+    df = df2
+    # Share of GDP held by lowest 10% for the latest date available, by country
+    lowest_10 = df[(df["Indicator Name"] == "Income share held by lowest 10%")].groupby(
+        "Country Code").last()
+    # GDP per capita for the latest date available, by country
+    lowest_10_gdp = df[(df["Indicator Name"] == "GDP per capita (current US$)")].groupby(
+        "Country Code").last()
+    # Income share held by lowest 10% multiplied by GDP per capita, by country
+    lowest_10["GDP per capita"] = lowest_10_gdp["value"]
+    lowest_10["Income share of lowest 10%"] = lowest_10["value"]
+    lowest_10["Share of GDP per capita of lowest 10%"] = lowest_10["Income share of lowest 10%"] / \
+        10 * \
+        lowest_10["GDP per capita"]  # divide by 10 to get the gdp per person in the lowest 10%
+
+
+    # Share of GDP held by highest 10% for the latest date available, by country
+    highest_10 = df[(df["Indicator Name"] == "Income share held by highest 10%")].groupby(
+        "Country Code").last()
+    # GDP per capita for the latest date available, by country
+    highest_10_gdp = df[(df["Indicator Name"] == "GDP per capita (current US$)")].groupby(
+        "Country Code").last()
+    # Income share held by highest 10% multiplied by GDP per capita, by country
+    highest_10["GDP per capita"] = highest_10_gdp["value"]
+    highest_10["Income share of highest 10%"] = highest_10["value"]
+    highest_10["Share of GDP per capita of highest 10%"] = highest_10["Income share of highest 10%"] / \
+        10 * \
+        highest_10["GDP per capita"]  # divide by 10 to get the gdp per person in the highest 10%
+
+    # Merge the two dataframes
+    merged = lowest_10.merge(highest_10, on="Country Code")
+    merged = merged[["Country Name_x", "Share of GDP per capita of lowest 10%",
+                    "Share of GDP per capita of highest 10%", "GDP per capita_x"]]
+    merged = merged.rename(
+        columns={"Country Name_x": "Country Name", "GDP per capita_x": "GDP per capita", 
+        "Share of GDP per capita of lowest 10%": "Lowest 10% in income", "Share of GDP per capita of highest 10%": "Highest 10% in income"})
+
+    merged["GDP Percentile"] = merged["GDP per capita"].rank(
+        pct=True).sort_values()
+
+    # Plot the data
+    # Figure size
+
+    # Plot a scatter plot GDP percentile on the x-axis and share of GDP per capita of lowest 10% and highest 10% on the y-axis using plotly.express
+    fig = px.scatter(merged, x="GDP Percentile", y=["Lowest 10% in income", "Highest 10% in income"],
+                    hover_name="Country Name",
+                    labels={
+                        "Share of GDP per capita of lowest 10%": "Lowest 10%", 
+                        "Share of GDP per capita of highest 10%": "Highest 10%", 
+                        "GDP Percentile": "GDP Percentile"})
+
+    # fix legend at top left
+    fig.update_layout(legend=dict(
+        yanchor="top",
+        y=0.99,
+        xanchor="left",
+        x=0.01
+    ))
+
+
+    # Axis labels
+    fig.update_xaxes(title_text="GDP Percentile")
+    fig.update_yaxes(title_text="GDP * income share")
+
+    # Line at 0.9 percentile
+    fig.add_shape(type="line", x0=0.9, y0=0, x1=0.9, y1=350000,
+                line=dict(color="gray", width=2))
+    fig.add_shape(type="line", x0=0.1, y0=0, x1=0.1, y1=350000,
+                line=dict(color="gray", width=2))
+
+
     html = fig.to_html(include_plotlyjs="require", full_html=False)
 
+    return html
+
+def income_share():
+    # Average income share held by the richest 10% in the world over time since the 2000s
+    highest_10 = df[(df["Indicator Name"] == "Income share held by highest 10%")].groupby("Date").mean()
+    highest_10 = highest_10.reset_index()
+    highest_10 = highest_10.rename(columns={"value": "Income share held by highest 10% (%)"})
+
+    # plot from 2000 to 2020
+    highest_10 = highest_10[(highest_10["Date"] >= 2000) & (highest_10["Date"] <= 2020)]
+    #plot
+    fig = px.line(highest_10, x="Date", y="Income share held by highest 10% (%)", title="Average income share held by the richest 10% in the world over time since the 2000s")
+    fig.update_xaxes(title_text="Year")
+
+    # To html
+    html = fig.to_html(include_plotlyjs="require", full_html=False)
+    
     return html
 
 def life_expectancy():
