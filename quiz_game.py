cdc6feb367e69d6e97a23236e9d8c5c0e9f9aa91
Norika Narimatsu <77351189+NorikaNarimatsu@users.noreply.github.com>
2022-11-18 16:53:49 -0300

    Update quiz_game.py

--- quiz_game.py
+++ quiz_game.py
@@ -37,7 +37,7 @@ if __name__ == '__main__':
     main()
     #start_server(main, debug=True, remote_access=True)
     
-    
+
 if __name__ == '__main__':
     import argparse
     from pywebio.platform.tornado_http import start_server as start_http_server
@@ -45,7 +45,15 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", "--port", type=int, default=8080)
+    parser.add_argument("--http", action="store_true", default=False, help='Whether to enable http protocol for communicates')
     args = parser.parse_args()
 
-    start_http_server(main, port=args.port)
+    if args.http:
+        start_http_server(main, port=args.port)
+    else:
+        # Since some cloud server may close idle connections (such as heroku),
+        # use `websocket_ping_interval` to  keep the connection alive
+        start_ws_server(main, port=args.port, websocket_ping_interval=30)
+        
+        
 
