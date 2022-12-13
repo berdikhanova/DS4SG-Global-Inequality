e674d7b26c1dd52c9cd4e6cc828cdada85eb7956
felipehlvo <felipehlvo@gmail.com>
2022-12-13 10:57:42 -0300

    changed logic of Play Again

--- Code/questions.py
+++ Code/questions.py
@@ -10,6 +10,7 @@ import random
 
 # import visualizations
 from Code.visualizations import *
+from quiz_game import *
 
 score = 0 
 n_questions = 0
@@ -268,6 +269,18 @@ In terms of global inequality, GDP per capita can be used as a rough indicator o
                     """
 }
 
+def first_page():
+    clear()
+    put_image("https://github.com/berdikhanova/DS4SG-Global-Inequality/blob/final_assignment/Quizality_logo.png?raw=true", width = '250px')
+    put_markdown('# Welcome to the Quizality!').style( 'text-align: center; margin: auto;  width: 80%; font-size: 40px') 
+    put_text('"Inequality" in itself is a term that can trigger a number of different ideas in the mind of the reader or listener based on their knowledge and prejudice. The meaning attached to ‘Economic Inequality’ is not self-explanatory. Individuals from developed countries often cannot envision the extent of deepening economic crises in the Global South. As students at an international university, we have had the unique opportunity to travel around the world and have witnessed firsthand the disparities in income, education, healthcare, and other socio-economic issues. Therefore, my peers and I signed up for a project for our tutorial class, ‘Data Science for Social Good’, for our Fall semester of 2022.')
+    put_markdown('# Meet Our Team!').style( 'text-align: center; margin: auto;  width: 80%; font-size: 40px')
+    put_image("https://github.com/berdikhanova/DS4SG-Global-Inequality/blob/final_assignment/team_photo.png?raw=true", width='500px').style( 'display: block; margin-left: auto;  margin-right: auto;  width: 60%;')
+    put_markdown('Are You Ready To Play?').style( 'text-align: center; margin: auto;  width: 80%; font-size: 40px') 
+    put_text('# In this game... there are two modes.........').style('text-align: center;')
+    put_markdown('## Choose your Game Mode').style('text-align: center;')
+    put_buttons(['Normal Mode', 'Infinite Mode'], onclick=[question1, infinite_mode]).style('text-align: center;')
+
 # Income in poor v rich countries
 def question1():
     make_question_checkbox(**question_dict["question1"])
@@ -318,6 +331,8 @@ def question5():
     clear()
     put_text("In Development")
 
+
+
 def last_page():
     global score
     global n_questions
@@ -336,7 +351,7 @@ def last_page():
 
     score = 0
     n_questions = 0
-    put_button("Play again", question1)
+    put_button("Play again", first_page)
 
 
     
