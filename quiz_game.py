a68bb8c1cecc93bbc084b5c2ee32344bdef5713b
felipehlvo <felipehlvo@gmail.com>
2022-12-16 16:11:34 -0300

    style update

--- quiz_game.py
+++ quiz_game.py
@@ -25,13 +25,32 @@ df = pd.read_csv("https://raw.githubusercontent.com/berdikhanova/DS4SG-Global-In
 
 #### MAKE LANDING PAGE ####
 #@use_scope('score', clear=True)
+CSS = """
+    * {margin: 0; padding: 0; font-family: "Roboto", sans-serif; font-size: 18px; text-align: left; }
+    """
 
+config(css_style=CSS)
+set_env(title='Quiz Game',output_max_width='90%')
 
 def main():
     first_page()
 
 if __name__ == '__main__':
-    main()
+    import argparse
+    from pywebio.platform.tornado_http import start_server as start_http_server
+    from pywebio import start_server as start_ws_server
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--port", type=int, default=8080)
+    parser.add_argument("--http", action="store_true", default=False, help='Whether to enable http protocol for communicates')
+    args = parser.parse_args()
+
+    if args.http:
+        start_http_server(main, port=args.port)
+    else:
+        # Since some cloud server may close idle connections (such as heroku),
+        # use `websocket_ping_interval` to  keep the connection alive
+        start_ws_server(main, port=args.port, websocket_ping_interval=30)
         
         
 
