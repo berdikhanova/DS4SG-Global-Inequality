2b76b56aa189f5bd55497b77cd8f2bb1ee3757f2
Marina Berdikhanova <marina@minerva.kgi.edu>
2022-12-16 20:35:11 +0500

    Updated the landing page

--- Code/questions.py
+++ Code/questions.py
@@ -371,14 +371,14 @@ def first_page():
     put_markdown('# Welcome to the Quizality!').style( 'text-align: center; margin: auto;  width: 80%; font-size: 40px') 
     # add team introduction
     put_text('"Inequality" in itself is a term that can trigger a number of different ideas in the mind of the reader or listener based on their knowledge and prejudice. The meaning attached to ‘Economic Inequality’ is not self-explanatory. Individuals from developed countries often cannot envision the extent of deepening economic crises in the Global South. As students at an international university, we have had the unique opportunity to travel around the world and have witnessed firsthand the disparities in income, education, healthcare, and other socio-economic issues. Therefore, my peers and I signed up for a project for our tutorial class, ‘Data Science for Social Good’, for our Fall semester of 2022.')
-    put_markdown('# Meet Our Team!').style( 'text-align: center; margin: auto;  width: 80%; font-size: 40px')
-    put_image("https://github.com/berdikhanova/DS4SG-Global-Inequality/blob/final_assignment/Resources/team_photo.png?raw=true", width='500px').style( 'display: block; margin-left: auto;  margin-right: auto;  width: 60%;')
     # add start buttons
     put_markdown('Are You Ready To Play?').style( 'text-align: center; margin: auto;  width: 80%; font-size: 40px') 
-    put_text('# In this game, there are two modes.........').style('text-align: center;')
+    put_text('In this game, there are two modes:').style('text-align: center;')
+    put_text('1. Normal Mode, where you learn new economic concepts and test your knowledge with diverse set of questions.').style('text-align: center;')
+    put_text('2. Infinite Mode to test your knowledge on how much your know about other contries across a variety of topics. Check your score and share with your friend!').style('text-align: center;')
     put_markdown('## Choose your Game Mode').style('text-align: center;')
     put_buttons(['Normal Mode', 'Infinite Mode'], onclick=[question1, infinite_mode]).style('text-align: center;')
-
+    put_image("https://github.com/berdikhanova/DS4SG-Global-Inequality/blob/final_assignment/Resources/team_photo.png?raw=true", width='500px').style( 'display: block; margin-left: auto;  margin-right: auto;  width: 60%;')
 # ECONOMIC
 # Income in poor v rich countries
 def question1():
